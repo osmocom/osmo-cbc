be8cbaf3fc15f44b61fa2c8e504a703aa0931c4a
Harald Welte <laforge@osmocom.org>
2019-12-03 21:54:35 +0100

    exit(2) on unsupported positional arguments on command line
    Change-Id: I701e6bffbfdfc98ff09b1d824e7707ef5a8b8059

--- src/cbc_main.c
+++ src/cbc_main.c
@@ -131,6 +131,11 @@ static void handle_options(int argc, char **argv)
 			break;
 		}
 	}
+
+	if (argc > optind) {
+		fprintf(stderr, "Unsupported positional arguments on command line\n");
+		exit(2);
+	}
 }
 
 static void signal_handler(int signal)
